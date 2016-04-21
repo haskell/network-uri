1941042e5af624d3aef7f810a27d213ebdcfa98e
Ezra elias kilty Cooper <ezra+github@ezrakilty.net>
2016-04-20 23:26:22 -0700

    Add some benchmarks for a few routines. This helps prove that changes do not regress performance significantly.

---
+++ tests/uri-bench.hs
@@ -0,0 +1,50 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# LANGUAGE BangPatterns #-}
+--------------------------------------------------------------------------------
+--  $Id: URITest.hs,v 1.8 2005/07/19 22:01:27 gklyne Exp $
+--
+--  Copyright (c) 2004, G. KLYNE.  All rights reserved.
+--  See end of this file for licence information.
+--------------------------------------------------------------------------------
+-- |
+--  Module      :  URITest
+--  Copyright   :  (c) 2004, Graham Klyne
+--  License     :  BSD-style (see end of this file)
+--
+-- Performance benchmarks for the network-uri package.
+--
+--------------------------------------------------------------------------------
+
+module Main where
+
+import Network.URI
+    ( parseURI
+    , parseURIReference
+    , pathSegments
+    , relativeFrom
+    , relativeTo
+    )
+
+import Criterion.Main
+import Control.DeepSeq
+
+main = defaultMain [
+    let Just !u = force (parseURI "http://ezample.org/foo/bar/baz//wimple/dimple/simple") in
+    bgroup "pathSegments" [
+       bench "head" $ nf head (pathSegments u)
+     , bench "tail" $ nf tail (pathSegments u)
+    ]
+  , bgroup "relativeFrom" [
+      let Just !u1 = force (parseURI "http://ex.it/foo/bar/baz/bop") in
+      let Just !u2 = force (parseURI "http://ex.it/foo/bar/baz/bap") in
+      bench "same 4" $ nf (relativeFrom u1) u2
+    , let Just !u1 = force (parseURI "http://ex.it/foo/bar/biz/bop") in
+      let Just !u2 = force (parseURI "http://ex.it/foo/bar/baz/bap") in
+      bench "different 4" $ nf (relativeFrom u1) u2
+    ]
+  , bgroup "relativeTo" [
+      let Just !u1 = force (parseURIReference "../../biz/../biz/./bop") in
+      let Just !u2 = force (parseURI "http://ex.it/foo/bar/baz/bap") in
+      bench "dots and double dots" $ nf (relativeTo u1) u2
+    ]
+  ]
